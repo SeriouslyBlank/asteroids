edbdee7239f98221f1a0f813377fbd2ae7f08cb9
Seriously Blank <siriusblackrc@gmail.com>
2025-05-25 15:45:47 +0530

    a triangle has been drawn if u run main

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	while 1 > 0:
+	x = SCREEN_WIDTH/2
+	y = SCREEN_HEIGHT/2
+	player = Player(x, y, PLAYER_RADIUS)
+
+	running = True
+	while running:
 		for event in pygame.event.get():
 		    if event.type == pygame.QUIT:
 		        return
 		screen.fill("black")
+		
+		player.draw(screen)
+
+		clock.tick(60)
+		dt = clock.tick(60) / 1000
+
 		pygame.display.flip()
 
 if __name__ == "__main__":
